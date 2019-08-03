4ef5e68177ec53bd1882ff6c539845c7e7018f8b
Kody Buss <kody.buss@gmail.com>
2019-08-03 01:58:08 -0500

    Refactored to minimize use of nested if and inverted logic
    Also removed dictionary and enums and replaced it with consts as it gave more flexibility to clean up the code.

--- src/ProviderQuality.Console/Program.cs
+++ src/ProviderQuality.Console/Program.cs
@@ -5,34 +5,21 @@ namespace ProviderQuality.Console
 {
     public class Program
     {
-        private enum AwardName
-        {
-            GovQualityPlus,
-            BlueFirst,
-            AcmePartnerFacility,
-            BlueDistinctionPlus,
-            BlueCompare,
-            TopConnectedProviders
-        }
+        private const string GovQualityPlus = "Gov Quality Plus";
+        private const string BlueFirst = "Blue First";
+        private const string AcmePartnerFacility = "ACME Partner Facility";
+        private const string BlueDistinctionPlus = "Blue Distinction Plus";
+        private const string BlueCompare = "Blue Compare";
+        private const string TopConnectedProviders = "Top Connected Providers";
 
-        readonly static Dictionary<AwardName, string> AwardNames = new Dictionary<AwardName, string>
+        public static IList<Award> Awards { get; set; } = new List<Award>
         {
-            { AwardName.GovQualityPlus, "Gov Quality Plus" },
-            { AwardName.BlueFirst, "Blue First"},
-            { AwardName.AcmePartnerFacility, "ACME Partner Facility" },
-            { AwardName.BlueDistinctionPlus, "Blue Distrinction Plus" },
-            { AwardName.BlueCompare, "Blue Compare" },
-            { AwardName.TopConnectedProviders, "Top Connected Providers" }
-        };
-
-        public IList<Award> Awards { get; set; } = new List<Award>
-        {
-            new Award { Name = AwardNames[AwardName.GovQualityPlus], SellIn = 10, Quality = 20 },
-            new Award { Name = AwardNames[AwardName.BlueFirst], SellIn = 2, Quality = 0 },
-            new Award { Name = AwardNames[AwardName.AcmePartnerFacility], SellIn = 5, Quality = 7 },
-            new Award { Name = AwardNames[AwardName.BlueDistinctionPlus], SellIn = 0, Quality = 80 },
-            new Award { Name = AwardNames[AwardName.BlueCompare], SellIn = 15, Quality = 20 },
-            new Award { Name = AwardNames[AwardName.TopConnectedProviders], SellIn = 3, Quality = 6 }
+            new Award { Name = GovQualityPlus, SellIn = 10, Quality = 20 },
+            new Award { Name = BlueFirst, SellIn = 2, Quality = 0 },
+            new Award { Name = AcmePartnerFacility, SellIn = 5, Quality = 7 },
+            new Award { Name = BlueDistinctionPlus, SellIn = 0, Quality = 80 },
+            new Award { Name = BlueCompare, SellIn = 15, Quality = 20 },
+            new Award { Name = TopConnectedProviders, SellIn = 3, Quality = 6 }
         };
 
         static void Main(string[] args)
@@ -40,9 +27,13 @@ namespace ProviderQuality.Console
             try
             {
                 System.Console.WriteLine("Updating award metrics...!");
+                PrintAwards();
 
                 Program app = new Program();
                 app.UpdateQuality();
+
+                System.Console.WriteLine("Successfully updated award metrics!");
+                PrintAwards();
 
                 System.Console.ReadKey();
             }
@@ -53,82 +44,77 @@ namespace ProviderQuality.Console
             }
         }
 
+        private static void PrintAwards()
+        {
+            foreach (Award award in Awards)
+                System.Console.WriteLine($"Award Name: {award.Name}, Award Sell In: {award.SellIn}, Award Quality: {award.Quality}");
+        }
+
         public void UpdateQuality()
         {
             foreach (Award award in Awards)
             {
-                if (award.Name != AwardNames[AwardName.BlueFirst] && award.Name != AwardNames[AwardName.BlueCompare])
-                {
-                    if (award.Quality > 0)
-                    {
-                        if (award.Name != AwardNames[AwardName.BlueDistinctionPlus])
-                        {
-                            award.Quality = award.Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (award.Quality < 50)
-                    {
-                        award.Quality = award.Quality + 1;
-
-                        if (award.Name == AwardNames[AwardName.BlueCompare])
-                        {
-                            if (award.SellIn < 11)
-                            {
-                                if (award.Quality < 50)
-                                {
-                                    award.Quality = award.Quality + 1;
-                                }
-                            }
-
-                            if (award.SellIn < 6)
-                            {
-                                if (award.Quality < 50)
-                                {
-                                    award.Quality = award.Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (award.Name != AwardNames[AwardName.BlueDistinctionPlus])
-                {
-                    award.SellIn = award.SellIn - 1;
-                }
-
-                if (award.SellIn < 0)
-                {
-                    if (award.Name != AwardNames[AwardName.BlueFirst])
-                    {
-                        if (award.Name != AwardNames[AwardName.BlueCompare])
-                        {
-                            if (award.Quality > 0)
-                            {
-                                if (award.Name != AwardNames[AwardName.BlueDistinctionPlus])
-                                {
-                                    award.Quality = award.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            award.Quality = award.Quality - award.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (award.Quality < 50)
-                        {
-                            award.Quality = award.Quality + 1;
-                        }
-                    }
-                }
+                CalculateInitialAwardQuality(award);
+                DecrementAwardSellIn(award);
+                if (award.SellIn >= 0) continue;
+                CalculatePostAwardQuality(award);
             }
         }
 
-    }
+        private static void CalculateInitialAwardQuality(Award award)
+        {
+            switch (award.Name)
+            {
+                case BlueFirst:
+                    IncrementAwardQuality(award);
 
+                    break;
+
+                case BlueCompare:
+                    IncrementAwardQuality(award);
+
+                    if (award.SellIn < 11) IncrementAwardQuality(award);
+                    if (award.SellIn < 6) IncrementAwardQuality(award);
+
+                    break;
+
+                default:
+                    DecrementAwardQuality(award);
+                    break;
+            }
+        }
+
+        private static void CalculatePostAwardQuality(Award award)
+        {
+            switch (award.Name)
+            {
+                case BlueFirst:
+                    IncrementAwardQuality(award);
+                    break;
+
+                case BlueCompare:
+                    award.Quality = 0;
+                    break;
+
+                default:
+                    DecrementAwardQuality(award);
+                    break;
+            }
+        }
+
+        private static void DecrementAwardSellIn(Award award)
+        {
+            if (award.Name != BlueDistinctionPlus) award.SellIn -= 1;
+        }
+
+        private static void DecrementAwardQuality(Award award)
+        {
+            if (award.Name != BlueDistinctionPlus && award.Quality > 0) award.Quality -= 1;
+        }
+
+        private static void IncrementAwardQuality(Award award)
+        {
+            if (award.Quality < 50) award.Quality += 1;
+        }
+    }
 }
